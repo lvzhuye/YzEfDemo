1b886410799d3682928ff8604b01bf0f19d5b67a
Yz <870887447@qq.com>
2017-12-27 17:37:19 +0800

    first commit

---
+++ YzEfDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YzEfDemo.Model;
+
+namespace YzEfDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testModel = new TestModel
+            {
+                Name="Yz",
+                Age=26
+            };
+            YzEfDemoDbContext yzEfDemoDbContext = new YzEfDemoDbContext();
+            yzEfDemoDbContext.TestModels.Add(testModel);
+
+            //同一个dbContext实例，未保存之前是否能查到，查不到
+            var queryResults = yzEfDemoDbContext.TestModels.Where(tm => tm.Age == 26).FirstOrDefault();
+
+
+
+            yzEfDemoDbContext.SaveChanges();
+
+            //发送sql请求吗？不会，缓存机制呢？
+            queryResults = yzEfDemoDbContext.TestModels.Where(tm => tm.Age == 26).FirstOrDefault();
+
+
+
+        }
+    }
+}
---
+++ YzEfDemo/YzEfDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YzEfDemo.Model;
+
+namespace YzEfDemo
+{
+    public class YzEfDemoDbContext:DbContext
+    {
+        public DbSet<TestModel> TestModels { get; set; }
+    }
+}
