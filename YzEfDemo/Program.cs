8dba1e88b49d4a99d03619e6322c6fb7cad6e1e7
Yz <870887447@qq.com>
2017-12-28 09:36:34 +0800

    update two modes test

--- YzEfDemo/Program.cs
+++ YzEfDemo/Program.cs
@@ -14,22 +14,41 @@ namespace YzEfDemo
             var testModel = new TestModel
             {
                 Name="Yz",
-                Age=26
+                Age=22
             };
             YzEfDemoDbContext yzEfDemoDbContext = new YzEfDemoDbContext();
             yzEfDemoDbContext.TestModels.Add(testModel);
 
-            //同一个dbContext实例，未保存之前是否能查到，查不到
-            var queryResults = yzEfDemoDbContext.TestModels.Where(tm => tm.Age == 26).FirstOrDefault();
+            //同一个dbContext实例，未保存之前是否能查到，查不到，不查缓存吗？
+            var queryResults = yzEfDemoDbContext.TestModels.Where(tm => tm.Age == 23).FirstOrDefault();
 
-
+            //不同dbContext实例，未保存之前能查到吗？查不到
+            YzEfDemoDbContext yzEfDemoDbContext2 = new YzEfDemoDbContext();
+            var queryResults2 = yzEfDemoDbContext2.TestModels.Where(tm => tm.Age == 22).FirstOrDefault();
 
             yzEfDemoDbContext.SaveChanges();
 
             //发送sql请求吗？不会，缓存机制呢？
-            queryResults = yzEfDemoDbContext.TestModels.Where(tm => tm.Age == 26).FirstOrDefault();
+            queryResults = yzEfDemoDbContext.TestModels.Where(tm => tm.Age == 22).FirstOrDefault();
+
+            //不同dbContext实例，另一个实例保存的，能查到吗？能查到
+            queryResults2 = yzEfDemoDbContext2.TestModels.Where(tm => tm.Age == 22).FirstOrDefault();
 
 
+            //更新实体两种方式，观察sql更新
+            //exec sp_executesql N'UPDATE [dbo].[TestModels]
+            //SET [Name] = @0
+            //WHERE ([Id] = @1)
+            //',N'@0 nvarchar(max) ,@1 int',@0=N'Yz2',@1=7
+            queryResults.Name = "Yz2";
+
+            //exec sp_executesql N'UPDATE [dbo].[TestModels]
+            //SET [Name] = @0, [Age] = @1
+            //WHERE ([Id] = @2)
+            //',N'@0 nvarchar(max) ,@1 int,@2 int',@0=N'Yz2',@1=22,@2=8
+            //yzEfDemoDbContext.Entry(queryResults).State = System.Data.Entity.EntityState.Modified;
+
+            yzEfDemoDbContext.SaveChanges();
 
         }
     }
